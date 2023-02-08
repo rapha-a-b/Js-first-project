5a3791b3a494ceb1e155d708e53bb618b22579fa
rapha-a-b <119280461+rapha-a-b@users.noreply.github.com>
2023-02-09 01:44:22 +0200

    js project first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let FristName = prompt("please enter your name");
+let mail = prompt("please enter your email");
+console.log(FristName + " " + mail);
+let gender = prompt("Are you Male or Female?").toUpperCase();
+if (gender === "MALE" || gender === "FEMALE") {
+  if (gender === "MALE") {
+    alert("ברוך הבא " + FristName);
+  } else if (gender === "FEMALE") {
+    alert("ברוכה הבאה " + FristName);
+  }
+} else {
+  alert("invalid Gender");
+}
+
+let newsletter = confirm("would you like to subscribe to our newsletter?");
+if (newsletter === true) {
+  alert("thank you your email :" + mail + "was registered");
+} else {
+  alert("your lose");
+}
+
+let userAge = prompt("what is your age?");
+userAge = Number(userAge);
+console.log(typeof userAge);
+if (typeof userAge !== "number") {
+  userAge = prompt("please enter your age using numbers only");
+}
+if (userAge <= 0) {
+  alert("you are not born yet");
+} else if (userAge < 13) {
+  alert("you are  a child");
+} else if (userAge < 18) {
+  alert("you are a teenager");
+} else if (userAge > 18) {
+  prompt("you are a Man/Woman");
+} else if (userAge >= 120) {
+  prompt("Hope you are in Heaven 👼");
+}
